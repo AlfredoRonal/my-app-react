6023385511675e49a3b0215a1bb761421ba4a6ae
AlfredoRonal <ronaldosccof@gmail.com>
2020-07-24 09:06:37 -0500

    Updates

---
+++ precache-manifest.adb12dca9203cc56de924bdaf541f31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e654d65400fba617a967a2f8aac5edf",
+    "url": "/my-app-react/index.html"
+  },
+  {
+    "revision": "1dd9f3f139e5698989fa",
+    "url": "/my-app-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "35d654758fd800b8db72",
+    "url": "/my-app-react/static/js/2.865ded5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react/static/js/2.865ded5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd9f3f139e5698989fa",
+    "url": "/my-app-react/static/js/main.eb328415.chunk.js"
+  },
+  {
+    "revision": "a3e0b63c540b7240091c",
+    "url": "/my-app-react/static/js/runtime-main.8f1ffed2.js"
+  }
+]);
